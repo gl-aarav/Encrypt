d4a498de7e54c17e4cf4f0b6f777357d072facd7
Aarav Goyal <gl.aarav@gmail.com>
2025-01-10 14:34:35 -0800

    Update Encrypt.java

--- Encrypt.java
+++ Encrypt.java
@@ -1,3 +1,4 @@
+
 /* Aarav Goyal
  * Encrypt.java
  * 1/8/2025
@@ -8,10 +9,12 @@ import java.util.Scanner;
 public class Encrypt 
 {
 	private String line;
+	private int timesEncrypted;
 
 	public Encrypt() 
 	{
 		line = "";
+		timesEncrypted = 0;
 	}
 
 	public static void main(String[] args) 
@@ -22,13 +25,39 @@ public class Encrypt
 
 	public void rotate13Times() 
 	{
+		int printTimes;
 		System.out.print("\n\n\nWelcome to Encrypt.java. Please enter a word, phrase, or sentence.\n\n-> ");
 		getString();
-		for (int i = 1; i <= 13; i++) 
-		{ // Loop 13 times
-			encryptString();
-			printString(i);
+		System.out.print("\nHow many times do you want to encrypt the code(1-26)\n\n-> ");
+		getInt();
+		if (timesEncrypted!=0 && timesEncrypted <= 26 && timesEncrypted > 0)
+		{
+			for (int i = 1; i <= timesEncrypted; i++) 
+			{ 
+				encryptString();
+				printString(i);
+			}
 		}
+		if (timesEncrypted!=0 && timesEncrypted > 26 && timesEncrypted > 0)
+		{
+			for (int i = 1; i <= timesEncrypted%26; i++) 
+			{ 
+				encryptString();
+				printString(i);
+			}
+		}
+		else if (timesEncrypted <0)
+		{
+			for (int i = 1; i <= 26; i++) 
+			{ 
+				encryptString();
+				printString(i);
+			}
+		}
+		else
+		{
+			System.out.println("\nEncryption 1:\n" + timesEncrypted);
+		}	
 		System.out.println("\n\n\n");
 	}
 
@@ -38,26 +67,32 @@ public class Encrypt
 		line = in.nextLine();
 	}
 
+	public void getInt()
+	{
+		Scanner in = new Scanner (System.in);
+		timesEncrypted = in.nextInt();
+	}
+
 	public void encryptString() 
 	{
-		String encrypted = ""; // Start with an empty string
-
+		String encrypted = "";
+	
 		for (int x = 0; x < line.length(); x++) 
 		{
 			char ch = line.charAt(x);
-
 			if (ch >= 'A' && ch <= 'Z') 
-				ch = (char) ((ch - 'A' + 2) % 26 + 'A');
+				ch = (char) ((ch - 'A' + 1) % 26 + 'A');
 			else if (ch >= 'a' && ch <= 'z') 
-				ch = (char) ((ch - 'a' + 2) % 26 + 'a');
+				ch = (char) ((ch - 'a' + 1) % 26 + 'a');
 			encrypted += ch;
 		}
+		
 
 		line = encrypted; 
 	}
 
 	public void printString(int number) 
 	{
-		System.out.println("\nEncryption " + number + ": \n" + line);
+		System.out.println("\nEncryption " + number + ":\n" + line);
 	}
 }
